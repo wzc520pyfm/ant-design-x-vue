f7491029640ad3dcdc7d99fb3f9c512d1cac48e5
inCBle <84701254+inCBle@users.noreply.github.com>
2025-04-16 22:37:05 +0800

    fix(thought-chain): Fixed thought chain component ellipsis not taking effect.

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,11 +1,49 @@
-<script setup lang="ts">
-import {Bubble} from "ant-design-x-vue";
+<script setup lang="tsx">
+import { MoreOutlined } from '@ant-design/icons-vue';
+import { Button, Card } from 'ant-design-vue';
+import { ThoughtChain, type ThoughtChainProps } from 'ant-design-x-vue';
+
+defineOptions({ name: 'AXThoughtChainBasic' });
+
+const items: ThoughtChainProps['items'] = [
+  {
+    title: 'Thought Chain Item Title',
+    description: 'description',
+    extra: <Button type="text" icon={<MoreOutlined />} />,
+    tooltip: {
+      titleConfig: {
+        title: '自定义 Tooltip 标题',
+      },
+      descriptionConfig: {
+        title: '自定义 Tooltip 副标题',
+      },
+    },
+  },
+  {
+    title: '配置不显示 description',
+    description: 'description',
+    extra: <Button type="text" icon={<MoreOutlined />} />,
+    tooltip: {
+      descriptionConfig: {
+        open: false,
+        title: '配置不显示 description ',
+      },
+    },
+  },
+  {
+    title: '隐藏 tooltip',
+    description: 'description',
+    extra: <Button type="text" icon={<MoreOutlined />} />,
+    // 设置不显示 tooltip
+    tooltip: false,
+  },
+];
+
+defineRender(() => {
+  return (
+    <Card style={{ width: '250px' }}>
+      <ThoughtChain items={items} />
+    </Card>
+  );
+});
 </script>
-
-<template>
-  <Bubble content="hello world" />
-</template>
-
-<style scoped>
-
-</style>
--- src/thought-chain/item.vue
+++ src/thought-chain/item.vue
@@ -1,28 +1,37 @@
 <script setup lang="tsx">
 import classnames from 'classnames';
-import { computed, toRefs, useId } from 'vue';
+import { computed, useId } from 'vue';
 import pickAttrs from '../_util/pick-attrs';
 import type { ThoughtChainNodeProps } from './interface';
 import { useThoughtChainNodeContextInject } from './context';
-import { Avatar, Typography } from 'ant-design-vue';
+import { Avatar, Typography, Tooltip, type TooltipProps } from 'ant-design-vue';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons-vue';
-import { TransitionCollapse } from '../transition-collapse'
+import { TransitionCollapse } from '../transition-collapse';
 
 defineOptions({ name: 'AXThoughtChainNode' });
 
-const { info = {}, nextStatus, onClick, ...restProps } = defineProps<ThoughtChainNodeProps>();
+const {
+  info = {},
+  nextStatus,
+  onClick,
+  ...restProps
+} = defineProps<ThoughtChainNodeProps>();
 
-const domProps = computed(() => pickAttrs(restProps, {
-  attr: true,
-  aria: true,
-  data: true,
-}));
+const domProps = computed(() =>
+  pickAttrs(restProps, {
+    attr: true,
+    aria: true,
+    data: true,
+  }),
+);
 
 // ================= ThoughtChainNodeContext ====================
 const thoughtChainNodeContext = useThoughtChainNodeContextInject();
 const prefixCls = computed(() => thoughtChainNodeContext.value.prefixCls);
 // const collapseMotion = computed(() => thoughtChainNodeContext.value.collapseMotion);
-const enableCollapse = computed(() => thoughtChainNodeContext.value.enableCollapse);
+const enableCollapse = computed(
+  () => thoughtChainNodeContext.value.enableCollapse,
+);
 const expandedKeys = computed(() => thoughtChainNodeContext.value.expandedKeys);
 const direction = computed(() => thoughtChainNodeContext.value.direction);
 const classNames = computed(() => thoughtChainNodeContext.value.classNames);
@@ -39,6 +48,39 @@ const content = computed(() => info.content);
 const footer = computed(() => info.footer);
 const status = computed(() => info.status);
 const description = computed(() => info.description);
+const tooltip = computed(() => {
+  const tooltipConfig = info.tooltip ?? true;
+  const placement = direction.value === 'rtl' ? 'topRight' : 'topLeft';
+  const titleConfig: TooltipProps = {
+    title: title.value,
+    placement,
+  };
+  const descriptionConfig: TooltipProps = {
+    title: description.value,
+    placement,
+  };
+
+  if (typeof tooltipConfig === 'boolean') {
+    return { titleConfig, descriptionConfig };
+  }
+
+  return {
+    titleConfig: {
+      ...titleConfig,
+      ...(tooltipConfig.titleConfig ? tooltipConfig.titleConfig : {}),
+    },
+    descriptionConfig: {
+      ...descriptionConfig,
+      ...(tooltipConfig.descriptionConfig
+        ? tooltipConfig.descriptionConfig
+        : {}),
+    },
+  };
+});
+
+const hideTooltip = computed(
+  () => typeof info.tooltip === 'boolean' && !info.tooltip,
+);
 
 // ============================ Style ============================
 const itemCls = computed(() => `${prefixCls.value}-item`);
@@ -48,7 +90,9 @@ const onThoughtChainNodeClick = () => onClick?.(key.value);
 
 // ============================ Content Open ============================
 const contentOpen = computed(() => expandedKeys.value?.includes(key.value));
-const contentVisible = computed(() => enableCollapse.value ? contentOpen.value : true);
+const contentVisible = computed(() =>
+  enableCollapse.value ? contentOpen.value : true,
+);
 
 defineRender(() => {
   return (
@@ -57,7 +101,9 @@ defineRender(() => {
       class={classnames(
         itemCls.value,
         {
-          [`${itemCls.value}-${status.value}${nextStatus ? `-${nextStatus}` : ''}`]: status.value,
+          [`${itemCls.value}-${status.value}${
+            nextStatus ? `-${nextStatus}` : ''
+          }`]: status.value,
         },
         restProps.class,
       )}
@@ -65,7 +111,10 @@ defineRender(() => {
     >
       {/* Header */}
       <div
-        class={classnames(`${itemCls.value}-header`, classNames.value.itemHeader)}
+        class={classnames(
+          `${itemCls.value}-header`,
+          classNames.value.itemHeader,
+        )}
         style={styles.value.itemHeader}
         onClick={onThoughtChainNodeClick}
       >
@@ -74,15 +123,13 @@ defineRender(() => {
         {/* Header */}
         <div
           class={classnames(`${itemCls.value}-header-box`, {
-            [`${itemCls.value}-collapsible`]: enableCollapse.value && content.value,
+            [`${itemCls.value}-collapsible`]:
+              enableCollapse.value && content.value,
           })}
         >
           {/* Title */}
           <Typography.Text
             strong
-            ellipsis={{
-              tooltip: { placement: direction.value === 'rtl' ? 'topRight' : 'topLeft', title: title.value },
-            }}
             // @ts-expect-error
             class={`${itemCls.value}-title`}
           >
@@ -99,57 +146,68 @@ defineRender(() => {
                   rotate={contentOpen.value ? 90 : 0}
                 />
               ))}
-            {title.value}
+            {hideTooltip.value ? (
+              title.value
+            ) : (
+              <Tooltip {...tooltip.value.titleConfig}>{title.value}</Tooltip>
+            )}
           </Typography.Text>
           {/* Description */}
           {description.value && (
             <Typography.Text
               // @ts-expect-error
               class={`${itemCls.value}-desc`}
-              ellipsis={{
-                tooltip: {
-                  placement: direction.value === 'rtl' ? 'topRight' : 'topLeft',
-                  title: description.value,
-                },
-              }}
               type="secondary"
             >
-              {description.value}
+              {hideTooltip.value ? (
+                title.value
+              ) : (
+                <Tooltip {...tooltip.value.descriptionConfig}>
+                  {description.value}
+                </Tooltip>
+              )}
             </Typography.Text>
           )}
         </div>
         {/* Extra */}
-        {extra.value && <div class={`${itemCls.value}-extra`}>{extra.value}</div>}
+        {extra.value && (
+          <div class={`${itemCls.value}-extra`}>{extra.value}</div>
+        )}
       </div>
       {/* Content */}
 
-    <TransitionCollapse prefixCls={prefixCls.value}>
-      {content.value && (
-        <div
-          v-show={contentVisible.value}
-          class={classnames(`${itemCls.value}-content`)}
-        >
+      <TransitionCollapse prefixCls={prefixCls.value}>
+        {content.value && (
           <div
-            class={classnames(`${itemCls.value}-content-box`, classNames.value.itemContent)}
-            style={styles.value.itemContent}
+            v-show={contentVisible.value}
+            class={classnames(`${itemCls.value}-content`)}
           >
-            {content.value}
+            <div
+              class={classnames(
+                `${itemCls.value}-content-box`,
+                classNames.value.itemContent,
+              )}
+              style={styles.value.itemContent}
+            >
+              {content.value}
+            </div>
           </div>
-        </div>
-      )}
-    </TransitionCollapse>
-
+        )}
+      </TransitionCollapse>
 
       {/* Footer */}
       {footer.value && (
         <div
-          class={classnames(`${itemCls.value}-footer`, classNames.value.itemFooter)}
+          class={classnames(
+            `${itemCls.value}-footer`,
+            classNames.value.itemFooter,
+          )}
           style={styles.value.itemFooter}
         >
           {footer.value}
         </div>
       )}
     </div>
-  )
+  );
 });
 </script>
