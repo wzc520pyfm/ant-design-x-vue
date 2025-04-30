796dd96fb0471696c4e7a05999b2f8c4dc705164
inCBle <1374902847@qq.com>
2025-04-30 16:29:30 +0800

    feat(docs/thought-chain): Added "tooltip" demo (#254)

---
+++ docs/examples-setup/thought-chain/tooltip.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { h } from 'vue';
+import { MoreOutlined } from '@ant-design/icons-vue';
+import { Button, Card } from 'ant-design-vue';
+import { ThoughtChain, type ThoughtChainProps } from 'ant-design-x-vue';
+
+defineOptions({ name: 'AXThoughtChainBasic' });
+
+const items: ThoughtChainProps['items'] = [
+  {
+    title: '显示 tooltip',
+    description: 'description',
+    extra: h(Button, { type: 'text', icon: h(MoreOutlined) }),
+    tooltip: true,
+  },
+  {
+    title: 'Thought Chain Item Title',
+    description: 'description',
+    extra: h(Button, { type: 'text', icon: h(MoreOutlined) }),
+    tooltip: {
+      titleConfig: {
+        title: '自定义 Tooltip 标题',
+      },
+      descriptionConfig: {
+        title: '自定义 Tooltip 副标题',
+      },
+    },
+  },
+  {
+    title: '配置不显示 description',
+    description: 'description',
+    extra: h(Button, { type: 'text', icon: h(MoreOutlined) }),
+    tooltip: {
+      descriptionConfig: {
+        open: false,
+        title: '配置不显示 description ',
+      },
+    },
+  },
+];
+</script>
+<template>
+  <Card style="width: 250px">
+    <ThoughtChain :items="items" />
+  </Card>
+</template>
---
+++ docs/examples/thought-chain/tooltip.vue
@@ -0,0 +1,48 @@
+<script setup lang="tsx">
+import { MoreOutlined } from '@ant-design/icons-vue';
+import { Button, Card } from 'ant-design-vue';
+import { ThoughtChain, type ThoughtChainProps } from 'ant-design-x-vue';
+
+defineOptions({ name: 'AXThoughtChainBasic' });
+
+const items: ThoughtChainProps['items'] = [
+  {
+    title: '显示 tooltip',
+    description: 'description',
+    extra: <Button type="text" icon={<MoreOutlined />} />,
+    tooltip: true,
+  },
+  {
+    title: 'Thought Chain Item Title',
+    description: 'description',
+    extra: <Button type="text" icon={<MoreOutlined />} />,
+    tooltip: {
+      titleConfig: {
+        title: '自定义 Tooltip 标题',
+      },
+      descriptionConfig: {
+        title: '自定义 Tooltip 副标题',
+      },
+    },
+  },
+  {
+    title: '配置不显示 description',
+    description: 'description',
+    extra: <Button type="text" icon={<MoreOutlined />} />,
+    tooltip: {
+      descriptionConfig: {
+        open: false,
+        title: '配置不显示 description ',
+      },
+    },
+  },
+];
+
+defineRender(() => {
+  return (
+    <Card style={{ width: '250px' }}>
+      <ThoughtChain items={items} />
+    </Card>
+  );
+});
+</script>
