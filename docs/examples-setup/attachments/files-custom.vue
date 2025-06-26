5f3178315df83326a27f75cfb2df9058f6ef89d8
kieranwv <kieranwme@gmail.com>
2025-06-26 17:45:06 +0800

    fix: add image icon

--- docs/examples-setup/attachments/files-custom.vue
+++ docs/examples-setup/attachments/files-custom.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { Flex } from 'ant-design-vue';
 import { Attachments } from 'ant-design-x-vue';
-import { VideoCameraFilled, AudioFilled, CopyFilled } from '@ant-design/icons-vue';
+import { VideoCameraFilled, AudioFilled, CopyFilled, FileImageFilled } from '@ant-design/icons-vue';
 import { h } from 'vue';
 
 defineOptions({ name: 'AXAttachmentFiles' });
@@ -10,7 +10,12 @@ const fileIcons = [
   {
     icon: h(CopyFilled),
     color: '#8c8c8c',
-    ext: ['xlsx', 'xls', 'md', 'mdx','pdf', 'ppt', 'pptx', 'doc', 'docx', 'zip', 'rar', '7z', 'tar', 'gz'],
+    ext: ['xlsx', 'xls', 'md', 'mdx', 'pdf', 'ppt', 'pptx', 'doc', 'docx', 'zip', 'rar', '7z', 'tar', 'gz'],
+  },
+  {
+    icon: h(FileImageFilled),
+    color: '#8c8c8c',
+    ext: ['png', 'jpg', 'jpeg', 'gif', 'bmp', 'webp', 'svg'],
   },
   {
     icon: h(VideoCameraFilled),
@@ -25,38 +30,38 @@ const fileIcons = [
 ];
 
 const filesList = [
-    {
-      uid: '1',
-      name: 'excel-file.xlsx',
-      size: 111111,
-    },
-    {
-      uid: '2',
-      name: 'image-file.png',
-      size: 333333,
-    },
-    {
-      uid: '3',
-      name: 'pdf-file.pdf',
-      size: 444444,
-    },
-    {
-      uid: '4',
-      name: 'video-file.mp4',
-      size: 666666,
-    },
-    {
-      uid: '5',
-      name: 'audio-file.mp3',
-      size: 777777,
-    },
-    {
-      uid: '6',
-      name: 'markdown-file.md',
-      size: 999999,
-      description: 'Custom description here',
-    },
-  ];
+  {
+    uid: '1',
+    name: 'excel-file.xlsx',
+    size: 111111,
+  },
+  {
+    uid: '2',
+    name: 'image-file.png',
+    size: 333333,
+  },
+  {
+    uid: '3',
+    name: 'pdf-file.pdf',
+    size: 444444,
+  },
+  {
+    uid: '4',
+    name: 'video-file.mp4',
+    size: 666666,
+  },
+  {
+    uid: '5',
+    name: 'audio-file.mp3',
+    size: 777777,
+  },
+  {
+    uid: '6',
+    name: 'markdown-file.md',
+    size: 999999,
+    description: 'Custom description here',
+  },
+];
 </script>
 
 <template>
